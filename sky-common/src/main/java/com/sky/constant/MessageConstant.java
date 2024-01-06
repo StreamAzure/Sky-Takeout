fb7a1d62807e1a01ac0022da3287fa5bd0fc5184
StreamAzure <1905105036@qq.com>
2024-01-06 17:01:17 +0800

    员工注册功能，用户已存在的异常处理

--- sky-common/src/main/java/com/sky/constant/MessageConstant.java
+++ sky-common/src/main/java/com/sky/constant/MessageConstant.java
@@ -24,4 +24,5 @@ public class MessageConstant {
     public static final String ORDER_STATUS_ERROR = "订单状态错误";
     public static final String ORDER_NOT_FOUND = "订单不存在";
 
+    public static final String ALREADY_EXISTS = "用户已存在";
 }
